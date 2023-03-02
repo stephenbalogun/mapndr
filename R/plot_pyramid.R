92e93bc2b8a6c1b72d3a61576ef6a210bac96186
Stephen Balogun <77954949+stephenbalogun@users.noreply.github.com>
2023-03-02 12:30:44 +0100

    add three more default arguments to `plot_pyramid`

--- R/plot_pyramid.R
+++ R/plot_pyramid.R
@@ -2,6 +2,10 @@
 #'
 #' @param fill the variable name in the data to be used to plot the pyramid
 #' @param age_group the variable name in the data that contains the different age-band
+#' @param age_bands a vector of age-bands contained in the data to be used for age-categorization, if different from
+#' c( "0-4", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65+")
+#' @param border logical (boolean). It determines if the individual bars should have a border or not
+#' @param border_color if the `border` argument is set to `TRUE`, the user can supplied a border color using any of the standard color formats
 #' @param sex the variable name that contains the sex (gender) in the supplied data. The `sex` should be in the "M" and "F" format as commonly seen on the NDR
 #' @inheritParams map_lgas
 #'
@@ -29,23 +33,44 @@ plot_pyramid <- function(
     fill,
     age_group = age_group,
     sex = sex,
+    age_bands = NULL,
     label = TRUE,
     cols = NULL,
     size = NULL,
+    border = TRUE,
+    border_color = NULL,
     interactive = FALSE) {
   my_cols <- c("F" = "#f5c1c1", "M" = "#c1f5f5")
 
+  age_cat <- c(
+    "0-4", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39",
+    "40-44", "45-49", "50-54", "55-59", "60-64", "65+"
+  )
+
+  data_age_group <- dplyr::distinct(.data, {{ age_group }}) |> dplyr::pull({{ age_group }})
+
+
   validate_pyramid(label, interactive, size, cols)
+
+
+  if (!is.null(age_bands) && !all(age_bands %in% data_age_group)) {
+    rlang::abort("The age_bands supplied is not the same as the unique entries in the data provided")
+  }
+
+  if (!is.logical(border)) {
+    rlang::abort("The value supplied to the `border` argument must be either `TRUE` or `FALSE`")
+  }
+
+  if (!border && !is.null(border_color)) {
+    rlang::inform("This plot border argument has been set to `FALSE` and the border color supplied will be ignored")
+  }
 
   plot <- .data |>
     dplyr::mutate(
       {{ fill }} := ifelse({{ sex }} == "F", {{ fill }} * -1, {{ fill }}),
       {{ age_group }} := factor(
         {{ age_group }},
-        levels = c(
-          "0-4", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39",
-          "40-44", "45-49", "50-54", "55-59", "60-64", "65+"
-        )
+        levels = age_bands %||% age_cat
       )
     ) |>
     ggplot2::ggplot(
@@ -53,17 +78,23 @@ plot_pyramid <- function(
         y = {{ age_group }},
         fill = {{ sex }}
       )
-    ) +
-    ggplot2::geom_bar(
-      ggplot2::aes(x = {{ fill }}),
-      stat = "identity",
-      color = "#777777"
-    ) +
-    ggplot2::theme_classic() +
-    ggplot2::scale_x_continuous(
-      labels = scales::label_dollar(prefix = "", style_negative = "parens")
-    ) +
-    ggplot2::scale_fill_manual(values = cols %||% my_cols)
+    )
+
+  if (border) {
+    plot <- plot +
+      ggplot2::geom_bar(
+        ggplot2::aes(x = {{ fill }}),
+        stat = "identity",
+        color = border_color %||% "#777777"
+      )
+  } else {
+    plot <- plot +
+      ggplot2::geom_bar(
+        ggplot2::aes(x = {{ fill }}),
+        stat = "identity"
+      )
+  }
+
 
   if (label) {
     plot <- plot +
@@ -78,9 +109,22 @@ plot_pyramid <- function(
       )
   }
 
+
   if (interactive) {
-    plotly::ggplotly(plot)
+    plotly::ggplotly(
+      plot +
+        ggplot2::theme_classic() +
+        ggplot2::scale_x_continuous(
+          labels = scales::label_dollar(prefix = "", style_negative = "parens")
+        ) +
+        ggplot2::scale_fill_manual(values = cols %||% my_cols)
+    )
   } else {
-    plot
+    plot +
+      ggplot2::theme_classic() +
+      ggplot2::scale_x_continuous(
+        labels = scales::label_dollar(prefix = "", style_negative = "parens")
+      ) +
+      ggplot2::scale_fill_manual(values = cols %||% my_cols)
   }
 }
