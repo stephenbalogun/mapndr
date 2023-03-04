f5d102509c56b32904091ea40dcfbe78ef3e5766
Stephen Balogun <77954949+stephenbalogun@users.noreply.github.com>
2023-03-04 11:10:41 +0100

    1. modify and update arguments. 2. update validation 3. standardize argument names

--- R/map_lgas2.R
+++ R/map_lgas2.R
@@ -2,7 +2,10 @@
 #'
 #' @inheritParams map_lgas
 #' @param bubble the variable name of the column to be used for the bubble (point) plot
-#'
+#' @param label_bubble logical (boolean), indicating if the values for the `bubble` data should be displayed
+#' @param label_bubble_color when supplied, this color will be used as font-color for the `bubble` label
+#' @param bubble_color a string of color. When this is supplied, it overides the default color
+#' @param size_bubble the font-size for the `bubble` labels in whole numbers. The default is 2
 #' @return a two-dimensional LGA-level map
 #' @export
 #'
@@ -10,7 +13,7 @@
 #'
 #' ## map the lga_data accompanying the package, filling by `prev_x` and bubbles by `incidence_x`
 #'
-#' map_lgas2(lga_data, fill = prev_x, bubble = incidence_x, label = TRUE)
+#' map_lgas2(lga_data, fill = prev_x, bubble = incidence_x, label_lga = TRUE)
 #'
 map_lgas2 <- function(
     .data,
@@ -18,21 +21,31 @@ map_lgas2 <- function(
     bubble,
     state = state,
     lga = lga,
-    label = FALSE,
-    cols = NULL,
-    size = NULL,
+    label_lga = TRUE,
+    label_fill = FALSE,
+    label_bubble = FALSE,
+    label_lga_color = NULL,
+    label_fill_color = NULL,
+    label_bubble_color = NULL,
+    fill_colors = NULL,
+    bubble_color = NULL,
+    size_lga = NULL,
+    size_fill = NULL,
+    size_bubble = NULL,
+    border_color = NULL,
     interactive = FALSE) {
   states <- dplyr::distinct(.data, {{ state }}) |> dplyr::pull({{ state }})
 
   fill_vec <- dplyr::select(.data, {{ fill }}) |> dplyr::pull({{ fill }})
 
-  validate_maps(label, interactive, size, cols)
-
-  if (!is.null(cols) && length(cols) > 1 && length(cols) != length(unique(fill_vec))) {
-    rlang::abort("The values supplied to `col` argument must be colors of length equal to the unique entries in the `fill` variable! Did you supply discrete colors to a continuous `fill` variable?")
-  }
-
   noise <- stats::runif(1, min = 0.01, max = 0.02)
+
+  validate_lga_maps(label_lga, label_fill, size_lga, size_fill, interactive)
+
+
+  if (!is.null(fill_colors) && length(fill_colors) > 1 && length(fill_colors) != length(unique(fill_vec))) {
+    rlang::abort("The values supplied to `fill_colors` argument must be colors of length equal to the unique entries in the `fill` variable! Did you supply discrete colors to a continuous `fill` variable?")
+  }
 
   df <- ndr_lgas(states) |>
     dplyr::left_join(
@@ -57,18 +70,31 @@ map_lgas2 <- function(
       lat2 = .data$lat + (2 * noise)
     )
 
-  if (!is.null(cols) && length(cols) == 1) {
+  if (!is.numeric(fill_vec)) {
+    lab_data <- df |>
+      dplyr::distinct(.data$state, .data$lga, {{ fill }}) |>
+      dplyr::left_join(
+        lab_data,
+        dplyr::join_by(
+          state == {{ state }},
+          lga == {{ lga }}
+        )
+      )
+  }
+
+  if (!is.null(fill_colors) && length(fill_colors) == 1) {
     p <- df |>
       ggplot2::ggplot() +
       ggplot2::geom_polygon(
         ggplot2::aes(.data$long, .data$lat, group = .data$lga),
-        fill = cols,
-        color = "black",
+        fill = fill_colors,
+        color = border_color %||% "black",
         linewidth = 0.8
       ) +
       ggplot2::geom_point(
         data = lab_data,
         ggplot2::aes(.data$long, .data$lat, size = {{ bubble }}),
+        color = bubble_color %||% "black",
         alpha = 0.4
       ) +
       ggplot2::coord_sf() +
@@ -81,11 +107,12 @@ map_lgas2 <- function(
       ) +
       ggplot2::geom_polygon(
         ggplot2::aes(fill = {{ fill }}),
-        color = "black"
+        color = border_color %||% "black"
       ) +
       ggplot2::geom_point(
         data = lab_data,
         ggplot2::aes(.data$long, .data$lat, size = {{ bubble }}),
+        color = bubble_color %||% "black",
         alpha = 0.4
       ) +
       ggplot2::coord_sf() +
@@ -94,18 +121,36 @@ map_lgas2 <- function(
   }
 
 
-  if (label) {
+  if (label_lga) {
     p <- p +
       ggplot2::geom_text(
         data = lab_data,
         ggplot2::aes(.data$long2, .data$lat2, label = .data$lga),
-        size = size %||% 2,
+        size = size_lga %||% 2,
+        color = label_lga_color %||% "black",
         check_overlap = TRUE
-      ) +
+      )
+  }
+
+  if (label_bubble) {
+    p <- p +
       ggplot2::geom_text(
         data = lab_data,
-        ggplot2::aes(.data$long, .data$lat, label = {{ bubble }}),
-        size = size %||% 2,
+        ggplot2::aes(.data$long, .data$lat, label = scales::comma({{ bubble }})),
+        size = size_bubble %||% 2,
+        color = label_bubble_color %||% "#ffffff",
+        check_overlap = TRUE
+      )
+  }
+
+
+  if (label_fill) {
+    p <- p +
+      ggplot2::geom_text(
+        data = lab_data,
+        ggplot2::aes(.data$long2 + noise, .data$lat2 + noise, label = {{ fill }}),
+        size = size_fill %||% 2,
+        color = label_fill_color %||% "black",
         check_overlap = TRUE
       )
   }
@@ -117,7 +162,7 @@ map_lgas2 <- function(
 
     p <- p +
       ggplot2::scale_fill_manual(
-        values = cols %||% col_select
+        values = fill_colors %||% col_select
       )
   } else if (is.numeric(fill_vec)) {
     p <- p + ggplot2::scale_fill_viridis_c(alpha = 0.5)
--- R/map_lgas3.R
+++ R/map_lgas3.R
@@ -1,8 +1,14 @@
 #' Plot LGA-level Map with Two Bubbles
 #'
 #' @inheritParams map_lgas
-#' @param light_b the variable name of the column to be used for the lighter (bigger) bubble
-#' @param dark_b the variable name of the column to be used for the darker (smaller) bubble
+#' @inheritParams map_lgas2
+#' @param light_bubble the variable name of the column to be used for the lighter (bigger) bubble
+#' @param dark_bubble the variable name of the column to be used for the darker (smaller) bubble
+#' @param light_bubble_color a string of color. When this is supplied, it overides the default color for the `light_bubble` plots
+#' @param dark_bubble_color a string of color. When this is supplied, it overides the default color for the `dark_bubble` plots
+#' @param label_dark_bubble logical (boolean), indicating if the values for the `dark_bubble` data should be displayed
+#' @param label_dark_bubble_color when supplied, this color will be used as font-color for the `dark_bubble` label
+#' @param size_dark_bubble the font-size for the `dark_bubble` labels in whole numbers. The default is 2
 #'
 #' @return a three-dimensional LGA-level map
 #' @export
@@ -11,27 +17,43 @@
 #'
 #' ## map the lga_data, filling by `prev_x` and light_b by `population` and dark_b by `incidence_x`
 #'
-#' map_lgas3(lga_data, fill = prev_x, light_b = population, dark_b = incidence_x, label = TRUE)
+#' map_lgas3(
+#' lga_data,
+#' fill = prev_x,
+#' light_bubble = population,
+#' dark_bubble = incidence_x,
+#' label_dark_bubble = TRUE
+#' )
 #'
 map_lgas3 <- function(
     .data,
     fill,
-    light_b,
-    dark_b,
+    light_bubble,
+    dark_bubble,
     state = state,
     lga = lga,
-    label = FALSE,
-    cols = NULL,
-    size = NULL,
+    label_lga = TRUE,
+    label_fill = FALSE,
+    label_dark_bubble = FALSE,
+    label_lga_color = NULL,
+    label_fill_color = NULL,
+    label_dark_bubble_color = NULL,
+    fill_colors = NULL,
+    light_bubble_color = NULL,
+    dark_bubble_color = NULL,
+    size_lga = NULL,
+    size_fill = NULL,
+    size_dark_bubble = NULL,
+    border_color = NULL,
     interactive = FALSE) {
   states <- dplyr::distinct(.data, {{ state }}) |> dplyr::pull({{ state }})
 
   fill_vec <- dplyr::select(.data, {{ fill }}) |> dplyr::pull({{ fill }})
 
-  validate_maps(label, interactive, size, cols)
+  validate_lga_maps(label_lga, label_fill, size_lga, size_fill, interactive)
 
-  if (!is.null(cols) && length(cols) > 1 && length(cols) != length(unique(fill_vec))) {
-    rlang::abort("The values supplied to `col` argument must be colors of length equal to the unique entries in the `fill` variable! Did you supply discrete colors to a continuous `fill` variable?")
+  if (!is.null(fill_colors) && length(fill_colors) > 1 && length(fill_colors) != length(unique(fill_vec))) {
+    rlang::abort("The values supplied to `fill_colors` argument must be colors of length equal to the unique entries in the `fill` variable! Did you supply discrete colors to a continuous `fill` variable?")
   }
 
   noise <- stats::runif(1, min = 0.01, max = 0.02)
@@ -53,8 +75,8 @@ map_lgas3 <- function(
       dplyr::across(
         tidyselect::where(is.numeric), mean
       ),
-      {{ dark_b }} := dplyr::first({{ dark_b }}),
-      {{ light_b }} := dplyr::first({{ light_b }}),
+      {{ dark_bubble }} := dplyr::first({{ dark_bubble }}),
+      {{ light_bubble }} := dplyr::first({{ light_bubble }}),
       .groups = "drop"
     ) |>
     dplyr::mutate(
@@ -63,27 +85,39 @@ map_lgas3 <- function(
     )
 
 
-  if (!is.null(cols) && length(cols) == 1) {
+  if (!is.numeric(fill_vec)) {
+    lab_data <- df |>
+      dplyr::distinct(.data$state, .data$lga, {{ fill }}) |>
+      dplyr::left_join(
+        lab_data,
+        dplyr::join_by(
+          state == {{ state }},
+          lga == {{ lga }}
+        )
+      )
+  }
+
+  if (!is.null(fill_colors) && length(fill_colors) == 1) {
     p <- df |>
       ggplot2::ggplot() +
       ggplot2::geom_polygon(
         ggplot2::aes(.data$long, .data$lat, group = .data$lga),
-        fill = cols,
-        color = "black"
+        fill = fill_colors,
+        color = border_color %||% "black"
       ) +
       ggplot2::geom_point(
         data = lab_data,
-        ggplot2::aes(.data$long, .data$lat, size = {{ light_b }}),
-        color = "#f5c1c1",
+        ggplot2::aes(.data$long, .data$lat, size = {{ light_bubble }}),
+        color = light_bubble_color %||% "#f5c1c1",
         show.legend = FALSE
       ) +
       ggplot2::geom_point(
         data = lab_data,
         ggplot2::aes(
           .data$long, .data$lat,
-          size = {{ dark_b }}
+          size = {{ dark_bubble }}
         ),
-        color = "#ae2234",
+        color = dark_bubble_color %||% "#ae2234",
         show.legend = FALSE
       ) +
       ggplot2::coord_sf() +
@@ -96,21 +130,21 @@ map_lgas3 <- function(
       ) +
       ggplot2::geom_polygon(
         ggplot2::aes(fill = {{ fill }}),
-        color = "black"
+        color = border_color %||% "black"
       ) +
       ggplot2::geom_point(
         data = lab_data,
-        ggplot2::aes(.data$long, .data$lat, size = {{ light_b }}),
-        color = "#f5c1c1",
+        ggplot2::aes(.data$long, .data$lat, size = {{ light_bubble }}),
+        color = light_bubble_color %||% "#f5c1c1",
         show.legend = FALSE
       ) +
       ggplot2::geom_point(
         data = lab_data,
         ggplot2::aes(
           .data$long, .data$lat,
-          size = {{ dark_b }}
+          size = {{ dark_bubble }}
         ),
-        color = "#ae2234",
+        color = dark_bubble_color %||% "#ae2234",
         show.legend = FALSE
       ) +
       ggplot2::coord_sf() +
@@ -119,24 +153,43 @@ map_lgas3 <- function(
   }
 
 
-  if (label) {
+  if (label_lga) {
     p <- p +
       ggplot2::geom_text(
         data = lab_data,
         ggplot2::aes(.data$long2, .data$lat2, label = .data$lga),
-        size = size %||% 2,
+        size = size_lga %||% 2,
+        color = label_lga_color %||% "black",
         check_overlap = TRUE
-      ) +
+      )
+  }
+
+
+  if (label_dark_bubble) {
+    p <- p +
       ggplot2::geom_text(
         data = lab_data,
         ggplot2::aes(
           .data$long, .data$lat,
-          label = round({{ dark_b }})
+          label = scales::comma(round({{ dark_bubble }}))
         ),
-        size = size %||% 2,
+        size = size_dark_bubble %||% 2,
+        color = label_dark_bubble_color %||% "#ffffff",
         check_overlap = TRUE
       )
   }
+
+  if (label_fill) {
+    p <- p +
+      ggplot2::geom_text(
+        data = lab_data,
+        ggplot2::aes(.data$long2 + noise, .data$lat2 + noise, label = {{ fill }}),
+        size = size_fill %||% 2,
+        color = label_fill_color %||% "black",
+        check_overlap = TRUE
+      )
+  }
+
 
   if (is.character(fill_vec) | is.factor(fill_vec)) {
     col_select <- my_cols(length(unique(fill_vec)))
@@ -145,7 +198,7 @@ map_lgas3 <- function(
 
     p <- p +
       ggplot2::scale_fill_manual(
-        values = cols %||% col_select
+        values = fill_colors %||% col_select
       )
   } else if (is.numeric(fill_vec)) {
     p <- p + ggplot2::scale_fill_viridis_c(alpha = 0.5)
