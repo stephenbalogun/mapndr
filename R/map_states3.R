e23d1c0252b024e10a9a95b24aab07d99a4bf570
Stephen Balogun <77954949+stephenbalogun@users.noreply.github.com>
2023-03-11 16:55:38 +0100

    Change "light and dark" to "big and "small". Use the same scale for "big and small" bubbles.

--- R/map_states3.R
+++ R/map_states3.R
@@ -14,52 +14,67 @@
 #'   dplyr::count(state, wt = estimate, name = "estimate") |>
 #'   dplyr::mutate(estimate_scaled = estimate * 0.8)
 #'
-#' map_states3(state_data, fill = estimate, light_bubble = estimate, dark_bubble = estimate_scaled)
+#' map_states3(state_data, fill = estimate, big_bubble = estimate, small_bubble = estimate_scaled)
 #'
 map_states3 <- function(
     .data,
     fill,
-    light_bubble,
-    dark_bubble,
+    big_bubble,
+    small_bubble,
     state = state,
     label_state = TRUE,
     label_fill = FALSE,
-    label_dark_bubble = FALSE,
+    label_small_bubble = TRUE,
     label_state_color = NULL,
     label_fill_color = NULL,
-    label_dark_bubble_color = NULL,
+    label_small_bubble_color = NULL,
     fill_colors = NULL,
-    light_bubble_color = NULL,
-    dark_bubble_color = NULL,
+    big_bubble_color = NULL,
+    small_bubble_color = NULL,
+    bubble_transparency = NULL,
     size_state = NULL,
     size_fill = NULL,
-    size_dark_bubble = NULL,
+    size_small_bubble = NULL,
     border_color = NULL,
     border_width = NULL,
     gradient = NULL,
     grad_direction = NULL,
     na_fill = NULL,
+    all_regions = FALSE,
     interactive = FALSE) {
-  states <- dplyr::distinct(.data, {{ state }}) |> dplyr::pull({{ state }})
+  states <- dplyr::pull(.data, {{ state }}) |> unique()
 
-  fill_vec <- dplyr::select(.data, {{ fill }}) |> dplyr::pull({{ fill }})
+  fill_vec <- dplyr::pull(.data, {{ fill }}) |> unique()
 
   noise <- stats::runif(1, min = 0.05, max = 0.1)
 
 
-  validate_state_maps(label_state, label_fill, size_state, size_fill, interactive)
+  validate_state_maps(label_state, label_fill, size_state, size_fill, all_regions, interactive)
 
 
   if (!is.null(fill_colors) && length(fill_colors) > 1 && length(fill_colors) != length(unique(fill_vec))) {
     rlang::abort("The values supplied to `fill_colors` argument must be colors of length equal to the unique entries in the `fill` variable! Did you supply discrete colors to a continuous `fill` variable?")
   }
 
-  df <- ndr_states(states) |>
-    dplyr::left_join(
-      .data,
-      dplyr::join_by(state == {{ state }}),
-      multiple = "all"
-    )
+  if (all(unique(naijR::lgas_nigeria$state) %in% states) || all_regions) {
+    df <- ndr_states() |>
+      dplyr::left_join(
+        .data,
+        dplyr::join_by(
+          state == {{ state }}
+        ),
+        multiple = "all"
+      )
+  } else {
+    df <- ndr_states(states) |>
+      dplyr::left_join(
+        .data,
+        dplyr::join_by(
+          state == {{ state }}
+        ),
+        multiple = "all"
+      )
+  }
 
   lab_data <- df |>
     dplyr::group_by(.data$state) |>
@@ -67,25 +82,29 @@ map_states3 <- function(
       dplyr::across(
         tidyselect::where(is.numeric), mean
       ),
-      {{ dark_bubble }} := dplyr::first({{ dark_bubble }}),
-      {{ light_bubble }} := dplyr::first({{ light_bubble }}),
+      {{ small_bubble }} := dplyr::first({{ small_bubble }}),
+      {{ big_bubble }} := dplyr::first({{ big_bubble }}),
       .groups = "drop"
     ) |>
     dplyr::mutate(
       long2 = .data$long + (2 * noise),
       lat2 = .data$lat + (2 * noise)
+    ) |>
+    tidyr::pivot_longer(
+      cols = c({{ big_bubble }}, {{ small_bubble }}),
+      names_to = "indicator",
+      values_to = "value"
     )
-
 
   if (!is.numeric(fill_vec)) {
     lab_data <- df |>
       dplyr::distinct(.data$state, {{ fill }}) |>
       dplyr::left_join(
         lab_data,
-        dplyr::join_by(state)
+        dplyr::join_by(state),
+        multiple = "all"
       )
   }
-
 
   if (!is.null(fill_colors) && length(fill_colors) == 1) {
     p <- df |>
@@ -93,49 +112,47 @@ map_states3 <- function(
       ggplot2::geom_polygon(
         ggplot2::aes(.data$long, .data$lat, group = .data$group),
         fill = fill_colors,
-        color = border_color %||% "black",
-        linewidth = border_width %||% 0.5
+        color = border_color %||% border_grey(),
+        linewidth = border_width %||% 0.3
       ) +
       ggplot2::geom_point(
         data = lab_data,
-        ggplot2::aes(.data$long, .data$lat, size = {{ light_bubble }}),
-        color = light_bubble_color %||% "#f5c1c1",
-        show.legend = FALSE
-      ) +
-      ggplot2::geom_point(
-        data = lab_data,
-        ggplot2::aes(.data$long, .data$lat, size = {{ dark_bubble }}),
-        color = dark_bubble_color %||% "#ae2234",
-        show.legend = FALSE
-      ) +
-      ggplot2::coord_sf() +
-      ggplot2::theme_void() +
-      ggplot2::scale_size_area(max_size = 10)
+        ggplot2::aes(.data$long, .data$lat, size = .data$value, color = .data$indicator),
+        alpha = bubble_transparency %||% 0.5,
+        show.legend = c("indicator" = TRUE)
+      )
   } else {
+    p <- df |>
+      ggplot2::ggplot(
+        ggplot2::aes(.data$long, .data$lat, group = .data$lga)
+      ) +
+      ggplot2::geom_polygon(
+        ggplot2::aes(fill = {{ fill }}),
+        color = border_color %||% border_grey(),
+        linewidth = border_width %||% 0.3
+      ) +
+      ggplot2::geom_point(
+        data = lab_data,
+        ggplot2::aes(.data$long, .data$lat, size = .data$value, color = .data$indicator),
+        alpha = bubble_transparency %||% 0.5,
+        show.legend = c("indicator" = TRUE)
+      )
+
     p <- df |>
       ggplot2::ggplot(
         ggplot2::aes(.data$long, .data$lat, group = .data$group)
       ) +
       ggplot2::geom_polygon(
         ggplot2::aes(fill = {{ fill }}),
-        color = border_color %||% "black",
-        linewidth = border_width %||% 0.5
+        color = border_color %||% border_grey(),
+        linewidth = border_width %||% 0.3
       ) +
       ggplot2::geom_point(
         data = lab_data,
-        ggplot2::aes(.data$long, .data$lat, size = {{ light_bubble }}),
-        color = light_bubble_color %||% "#f5c1c1",
-        show.legend = FALSE
-      ) +
-      ggplot2::geom_point(
-        data = lab_data,
-        ggplot2::aes(.data$long, .data$lat, size = {{ dark_bubble }}),
-        color = dark_bubble_color %||% "#ae2234",
-        show.legend = FALSE
-      ) +
-      ggplot2::coord_sf() +
-      ggplot2::theme_void() +
-      ggplot2::scale_size_area(max_size = 10)
+        ggplot2::aes(.data$long, .data$lat, size = .data$value, color = .data$indicator),
+        alpha = bubble_transparency %||% 0.5,
+        show.legend = c("indicator" = TRUE)
+      )
   }
 
 
@@ -145,21 +162,22 @@ map_states3 <- function(
         data = lab_data,
         ggplot2::aes(.data$long2, .data$lat2, label = .data$state),
         size = size_state %||% 3,
-        color = label_state_color %||% "black",
+        color = label_state_color %||% label_grey(),
         check_overlap = TRUE
       )
   }
 
-  if (label_dark_bubble) {
+  if (label_small_bubble) {
     p <- p +
       ggplot2::geom_text(
-        data = lab_data,
+        data = lab_data |> dplyr::filter(.data$indicator == unique(lab_data$indicator)[[2]]),
         ggplot2::aes(
-          .data$long, .data$lat,
-          label = scales::comma({{ dark_bubble }})
+          .data$long,
+          .data$lat,
+          label = ifelse(any(.data$value < 1), .data$value, scales::comma(.data$value))
         ),
-        size = size_dark_bubble %||% 2,
-        color = label_dark_bubble_color %||% "#ffffff",
+        size = size_small_bubble %||% 2,
+        color = label_small_bubble_color %||% "#ffffff",
         check_overlap = TRUE
       )
   }
@@ -174,33 +192,44 @@ map_states3 <- function(
           label = {{ fill }}
         ),
         size = size_fill %||% 3,
-        color = label_fill_color %||% "black",
+        color = label_fill_color %||% label_grey(),
         check_overlap = TRUE
       )
   }
 
   if (is.character(fill_vec) | is.factor(fill_vec)) {
-    col_select <- my_cols(length(unique(fill_vec)))
+    col_select <- my_cols(length(fill_vec))
 
-    col_select <- stats::setNames(col_select, sort(unique(fill_vec)))
+    col_select <- stats::setNames(col_select, sort(fill_vec))
 
     p <- p +
       ggplot2::scale_fill_manual(
         values = fill_colors %||% col_select,
-        na.value = na_fill %||% "pink"
+        na.value = na_fill %||% off_white()
       )
   } else if (is.numeric(fill_vec)) {
     p <- p + ggplot2::scale_fill_viridis_c(
       alpha = 0.5,
       option = gradient %||% "E",
-      na.value = na_fill %||% "pink",
+      na.value = na_fill %||% off_white(),
       direction = grad_direction %||% -1
     )
   }
 
   if (interactive) {
-    plotly::ggplotly(p)
+    plotly::ggplotly(
+      p +
+        ggplot2::coord_sf() +
+        ggplot2::theme_void() +
+        ggplot2::scale_size_area(max_size = 20, guide = "none") +
+        ggplot2::scale_color_viridis_d() +
+        ggplot2::guides(color = ggplot2::guide_legend(override.aes = list(size = 5)))
+    )
   } else {
-    p
+    p +
+      ggplot2::coord_sf() +
+      ggplot2::theme_void() +
+      ggplot2::scale_size_area(max_size = 20, guide = "none") +
+      ggplot2::guides(color = ggplot2::guide_legend(override.aes = list(size = 5)))
   }
 }
