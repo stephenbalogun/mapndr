c7436502f5979d8db6cd1922617a63b1d277bdb4
Stephen Balogun <77954949+stephenbalogun@users.noreply.github.com>
2023-03-17 23:51:41 +0100

    optmise

--- R/cols.R
+++ R/cols.R
@@ -10,18 +10,6 @@
 my_cols <- function(n) {
   cols <- c("#ff6666", "#f5c1c1", "#ffe6e6", "#ffcc99", "#e6e600", "#cae8cb", "#c1f5f5", "#1ac6ff")
 
-  if (is.numeric(n) && n < 2) {
-    rlang::abort("Too little categories. Choose `n` between 2 and 7")
-  }
-
-  if (is.numeric(n) && n > 7) {
-    rlang::abort("Too many categories in the data. Choose `n` between 2 and 7 categories")
-  }
-
-  if (!is.numeric(n) || nchar(n) != 1) {
-    rlang::abort("`n` must be an integer between 2 and 7")
-  }
-
   switch(n,
     "Too little. Choose between 2 and 7 categories",
     c(cols[c(2, 6)]),
@@ -30,7 +18,7 @@ my_cols <- function(n) {
     c(cols[c(1, 3, 5, 6, 8)]),
     c(cols[c(1, 2, 4, 5, 6, 8)]),
     c(cols[c(1, 2, 4, 5, 6, 7, 8)]),
-    "Too many categories in the data. Choose between 2 and 7 categories"
+    "Invalid entry. Choose a number between 2 and 7 categories"
   )
 }
 
